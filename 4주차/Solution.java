8a845874bb4143466add00ff62361d35857bcb84
선민기 <suns1502@gmail.com>
2019-12-01 09:24:45 +0900

    반쪽짜리

--- 4주차/Solution.java
+++ 4주차/Solution.java
@@ -3,14 +3,19 @@ package dp;
 class Solution {
     public int solution(int[] money) {
         int maxanswer = 0;
-        int[] answer = {0,0};
+        int[] answer = {money[0],money[1]};
 
-        for(int i = 0;i < money.length;i++){
+        for(int i = 2;i < money.length;i++){
             answer[i%2] += money[i];
-            if(answer[i%2] > answer[(i+1)%2])
-                maxanswer = answer[i%2];
+            maxanswer = answer[i%2]= getMax(answer[i%2],answer[(i-1)%2]);
         }
 
         return maxanswer;
+    }
+
+    private int getMax(int x,int y){
+        if(x > y)
+            return x;
+        return y;
     }
 }
