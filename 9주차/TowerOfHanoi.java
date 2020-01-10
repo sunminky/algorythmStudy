368b798acc60272d302f834f903b57d8b1aaa6e0
sunminky <suns1502@gmail.com>
2020-01-10 19:10:45 +0900

    9주차

---
+++ 9주차/TowerOfHanoi.java
@@ -0,0 +1,40 @@
+package week9;
+
+import java.util.Scanner;
+
+public class TowerOfHanoi {
+    static String resultStr = "";
+    static int resultCnt = 0;
+    public static void main(String[] args) {
+        int towerHeight = new Scanner(System.in).nextInt();
+        new TowerOfHanoi().move(1,3,towerHeight);
+        System.out.println(resultCnt);
+        System.out.print(resultStr);
+    }
+    public void move(int start,int end,int phase){
+        if(phase == 1) {  //1 하나만 옮기면 되는 상황
+            //System.out.println(start + " " + end);
+            resultStr += new Integer(start).toString() + " " + new Integer(end).toString() + "\n";  //경로 변경 내역 추가
+            resultCnt++;    //옮긴 횟수 증가
+        }
+        else{
+            int 선택안된친구 = notSelected(start,end);
+            move(start,선택안된친구,phase-1);
+            move(start,end,1);
+            move(선택안된친구,end,phase-1);
+        }
+    }
+    public int notSelected(int p1,int p2){  //1,2,3 중 p1,p2가 아닌 것은?
+        boolean[] lst = new boolean[3];
+        int result = -1;
+
+        lst[p1-1] = true;
+        lst[p2-1] = true;
+        for(int i = 0;i < lst.length;i++)
+            if(lst[i] == false){
+                result = i+1;
+                break;
+            }
+        return result;
+    }
+}
