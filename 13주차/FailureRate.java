51c631d9d8f379b7db0d6d50399f4a8b65480bd2
sunminky <suns1502@gmail.com>
2020-02-16 12:36:09 +0900

    13주차

--- 13주차/FailureRate.java
+++ 13주차/FailureRate.java
@@ -16,19 +16,19 @@ public class FailureRate {
         int match = 0;
         int participant = stages.length;
 
-        for(int i = 0;i<rateArr.length;i++){
+        for(int i = 0;i<rateArr.length;i++){    //0번째 배열에는 인덱스 넣어줌
             rateArr[i][0] = i+1;
         }
-        for(int i = 1;i<=N;i++){
+        for(int i = 1;i<=N;i++){    //1~N번째 스테이지 까지
             match = 0;
-            for(int j = 0;j < stages.length;j++){
+            for(int j = 0;j < stages.length;j++){   //현재 스테이지에서 멈춘 사람 찾기
                 if(stages[j] == i) {  //현재 스테이지 못깬사람 찾기
                     match++;
                 }
             }
-            rateArr[i-1][1] = (double)match/participant;
-            participant = participant - match;
-            if(participant < 1)
+            rateArr[i-1][1] = (double)match/participant;    //실패율
+            participant = participant - match;              //다음 스테이지 도달 못한 사람이니까 빼기
+            if(participant < 1) //분모가 0이면 break
                 break;
         }
         Arrays.sort(rateArr, new Comparator<double[]>() {
@@ -40,7 +40,7 @@ public class FailureRate {
             }
         });
 
-        for(int i = 0;i<answer.length;i++){
+        for(int i = 0;i<answer.length;i++){ //answer에 집어넣기
             answer[i] = (int)rateArr[i][0];
         }
         return answer;
--- 13주차/SecretMap.java
+++ 13주차/SecretMap.java
@@ -16,18 +16,18 @@ public class SecretMap {
         answer = drawWall(map1,map2,n);
         return answer;
     }
-    public int[][] flatten(int[] map,int size){
+    public int[][] flatten(int[] map,int size){ //1차원 지도를 2차원으로 만듬
         int ret[][] = new int[size][size];
 
         for(int i = 0;i < size;i++){
-            String bin = Integer.toBinaryString(map[i]);
+            String bin = Integer.toBinaryString(map[i]);    //2진수 변환
             for(int j = 0;j < bin.length();j++){
-                ret[i][size-bin.length()+j] = bin.charAt(j)-48;
+                ret[i][size-bin.length()+j] = bin.charAt(j)-48; //char 반환해서 48빼줘야 숫자 나옴
             }
         }
         return ret;
     }
-    public String maptoString(int[] map){
+    public String maptoString(int[] map){   //0이면 " " 1이면 # 넣음
         StringBuilder ret = new StringBuilder();
         for(int i = 0;i < map.length;i++){
             if(map[i] == 0)
@@ -37,15 +37,15 @@ public class SecretMap {
         }
         return ret.toString();
     }
-    public String[] drawWall(int[][] map1,int[][] map2,int size){
+    public String[] drawWall(int[][] map1,int[][] map2,int size){   //1번지도와 2번지도 합쳐서 벽있는 부분 표시
         int combination[][] = new int[size][size];
         String[] ret = new String[size];
 
         for(int i = 0;i < size;i++){
             for(int j = 0;j < size;j++){
-                combination[i][j] = map1[i][j] | map2[i][j];
+                combination[i][j] = map1[i][j] | map2[i][j];    //둘 중 하나라도 벽이면 벽이다
             }
-            ret[i] = maptoString(combination[i]);
+            ret[i] = maptoString(combination[i]);               //각 행을 #과 " "로 이루어진 문자열로 만듬
         }
         return ret;
     }
