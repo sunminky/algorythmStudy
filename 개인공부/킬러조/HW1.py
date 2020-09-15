ea74d73e120e38d3b64f8da2dafa26ae6550501d
선민기 <suns1502@gmail.com>
2020-09-15 23:04:35 +0900

    Revert "봐라"
    This reverts commit 8ea4d5abf04e3259d79422417374d6994ba7ed26.

--- 개인공부/킬러조/HW1.py
+++
@@ -1,9 +0,0 @@
-#반지름 r이 주어졌을때 부피구하기
-
-import math
-
-if __name__ == '__main__':
-    radius = 4  #구의 반지름
-    volume = 4/3 * math.pi * radius**3   #구의 부피, V = 4/3 * pi * r**3
-    volume = round(volume, 2)   #소수점 2째자리까지 반올림
-    print("반지름 : {radius}\n부피 : {volume}".format(radius=radius, volume=volume))
