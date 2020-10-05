da402634a583d5f035c047c0040158b725090002
sunminky <suns1502@gmail.com>
2020-10-05 11:29:33 +0900

    알쓰12주차

--- 개인공부/알고리즘 스터디/12주차/Autocomplete.py
+++ 개인공부/알고리즘 스터디/12주차/Autocomplete.py
@@ -1,4 +1,6 @@
 #https://programmers.co.kr/learn/courses/30/lessons/17685?language=python3
+import sys
+
 class Trie:
     def __init__(self):
         self.childNode = dict()
@@ -14,18 +16,22 @@ class Trie:
         self.childNode[word[0]].add(word[1:])   #있으면 그 뒤 문자 추가
 
 
-    def refer(self, word, cnt, mini):
+    def refer(self, word, cnt):
         if len(word) == 0:   #단어가 끝났다면 종료
-            return mini
+            if len(self.childNode) > 1: #단어를 전부써야만 구별되는 경우
+                return cnt, False
+            return cnt, True
 
-        if self.childNode.get("end", False) and len(self.childNode) == 1:   #종료노드인 경우
-            return mini
+        #반환값 확인, 자식노드가 1보다 크면 바로 리턴
+        ret_val, flag = self.childNode[word[0]].refer(word[1:], cnt+1)
+        if flag and len(self.childNode) > 1: #자식노드가 1보다 큼, 구별되지 않는 경우 생기는 경우
+            flag = False
+            ret_val = cnt+1
 
-        # 더이상 매칭되는 문자가 없을 때...
-
-        return self.childNode[word[0]].refer(word[1:], cnt+1, mini)
+        return ret_val, flag
 
 def solution(words) :
+    sys.setrecursionlimit(1000001)  #스택깊이 제한 해제
     answer = 0
     rootNode = Trie()
 
@@ -34,13 +40,13 @@ def solution(words) :
         rootNode.add(element)
 
     for element in words:
-        answer += rootNode.refer(element, 0, 0)
+        answer += rootNode.refer(element, 0)[0]
 
     return answer
 
 if __name__ == '__main__':
-    #result = solution(["go","gone","guild"])
-    #print(result)
+    result = solution(["go","gone","guild"])
+    print(result)
 
     result = solution(["word", "war", "warrior", "world"])
     print(result)
