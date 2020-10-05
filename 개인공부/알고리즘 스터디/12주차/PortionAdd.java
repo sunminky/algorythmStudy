1ac7220b7a6dc0d8a704b3b25c3e94f649396acd
선민기 <suns1502@gmail.com>
2020-10-05 18:46:29 +0900

    알쓰12주차

--- 개인공부/알고리즘 스터디/12주차/PortionAdd.java
+++ 개인공부/알고리즘 스터디/12주차/PortionAdd.java
@@ -1,29 +1,69 @@
-package FenwickTree;
+/*https://www.acmicpc.net/problem/2042*/
+package HomeWork;
 
 import java.util.Scanner;
 
 public class PortionAdd {
+    long[] tree = null;
+
     public static void main(String[] args) {
         new PortionAdd().solution();
     }
+
     private void solution(){
         Scanner scanner = new Scanner(System.in);
         int indice = scanner.nextInt();
         int tries = scanner.nextInt() + scanner.nextInt();
-        int[] tree =new int[indice+1];
+        long[] elements = new long[indice+1];
+        tree =new long[indice+1];
 
+        /*트리 초기화*/
         for(int i=0;i<indice;i++){
-            tree[i+1] = scanner.nextInt();
-            System.out.print(tree[i+1] + " ");
+            long newValue = scanner.nextLong();
+            update(i+1, newValue);
+            elements[i+1] = newValue;
         }
-        System.out.println();
 
-        for(int i=0;i<tries;i++){
+        for(int i=0;i<tries;i++) {
             int action = scanner.nextInt();
             int b = scanner.nextInt();
-            int c = scanner.nextInt();
+            long c = scanner.nextLong();
 
-            System.out.println(action + ", " + b + ", " + c);
+            switch (action) {
+                case 1: //숫자 바꾸기
+                    long margin = c - elements[b];
+                    elements[b] = c;
+                    update(b, margin);
+                    break;
+                case 2: //부분 합 구하기
+                    System.out.println(sum((int)c) - sum(b-1));
+                    break;
+            }
+
         }
+    }
+
+    private void update(int idx, long newValue){
+        int grpBit = idx;
+        while (grpBit < tree.length - 1){
+            tree[grpBit] += newValue;
+            grpBit += grpBit & (-grpBit);
+        }
+        //전체 합에 대해서 업데이트
+        tree[tree.length - 1] += newValue;
+    }
+
+    private long sum(int idx){
+        int grpBit = idx;
+        int ret = 0;
+
+        if(idx == tree.length-1)
+            return tree[tree.length-1];
+
+        while (grpBit > 0){
+            ret += tree[grpBit];
+            grpBit -= grpBit & (-grpBit);
+        }
+        return ret;
     }
 }
