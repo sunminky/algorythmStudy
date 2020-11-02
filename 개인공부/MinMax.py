d356b36d08fd730542dcf91fb31f59dfe3721047
sunminky <suns1502@gmail.com>
2020-11-02 17:22:14 +0900

    개인공부

--- 개인공부/MinMax.py
+++ 개인공부/MinMax.py
@@ -1,3 +1,5 @@
 #https://programmers.co.kr/learn/courses/30/lessons/12939?language=python3
 def solution(s):
-    return str(min(list(map(int,s.split())))) + " " + str(max(list(map(int,s.split()))))
+    lst = list(map(int,s.split()))
+    lst.sort()
+    return "{} {}".format(lst[0], lst[-1])
