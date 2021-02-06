0e7ebc5421d49ff2116cbeca0347be20c88ed638
선민기 <suns1502@gmail.com>
2021-02-06 22:18:26 +0900

    알쓰 11주차

--- 개인공부/새 스터디/11주차/MenuRenewal.py
+++ 개인공부/새 스터디/11주차/MenuRenewal.py
@@ -1,6 +1,7 @@
 #https://programmers.co.kr/learn/courses/30/lessons/72411
 
-from itertools import combinations
+from itertools import combinations  #중복제외 순열
+#from itertools import permutations  #중복포함 순열
 
 def solution(orders, course):
 
@@ -10,24 +11,24 @@ def solution(orders, course):
 
     for c in course:
         for o in orders:
-            if len(o) >= c:
-                for s in combinations(sorted(o), c):
-                    if not dictionary[c].get("".join(s), False):
-                        dictionary[c]["".join(s)] = 1
+            if len(o) >= c: #n개이상의 코스는 n개 이상의 주문을 한 사람한테서만 조사
+                for s in combinations(sorted(o), c):    #오더가 항상 오름차순으로 오는것은 아님
+                    combi = "".join(s)  #조합
+                    if not dictionary[c].get(combi, False):
+                        dictionary[c][combi] = 1
                     else:
-                        dictionary[c]["".join(s)] += 1
-                    if dictionary[c]["".join(s)] > maxValue[c]:
-                        maxValue[c] = dictionary[c]["".join(s)]
+                        dictionary[c][combi] += 1
+                    if dictionary[c][combi] > maxValue[c]: #가장많이 주문된 코스 저장
+                        maxValue[c] = dictionary[c][combi]
 
     for c in course:
-        if maxValue[c] < 2:
+        if maxValue[c] < 2: #가장 많이 시킨 코스가 2보다 작으면 패스
             continue
         for item in dictionary[c].keys():
             if maxValue[c] == dictionary[c][item]:
-                answer.append(item)
+                answer.append(item) #가장 많이 시킨 코스들 저장
 
-    answer.sort()
-    print(answer)
+    answer.sort()   #정렬
     return answer
 
 if __name__ == '__main__':
--- 개인공부/새 스터디/11주차/NewId.py
+++ 개인공부/새 스터디/11주차/NewId.py
@@ -6,11 +6,11 @@ def solution(new_id):
     #1단계
     answer = new_id.lower()
     #2단계
-    answer = re.sub('[^a-z0-9-_.]', '', answer)
+    answer = re.sub('[^a-z0-9-_.]', '', answer) #알파벳, 숫자, - , _ , .을 제외(^)하고는 모두 ''로 만듬
     #3단계
-    answer = re.sub('[.]{1,}', '.', answer)
+    answer = re.sub('[.]{1,}', '.', answer) #.이 1번이상 반복됬으면 .로 바꿈
     #4단계
-    answer = re.sub('^[.]|[.]$',"", answer)
+    answer = re.sub('^[.]|[.]$',"", answer) #.으로 시작(^)하거나 .으로 끝나($)면 .을 제거
     #5단계
     if not answer:
         answer = "a"
