d84908e7844954b9144ce6cdddac8bcea1f7d129
선민기 <suns1502@gmail.com>
2021-02-06 17:18:19 +0900

    알쓰 9주차

--- 개인공부/새 스터디/9주차/ConferenceRoom.py
+++ 개인공부/새 스터디/9주차/ConferenceRoom.py
@@ -3,5 +3,28 @@
 import sys
 
 if __name__ == '__main__':
+    conference = []
+
     for _ in range(int(sys.stdin.readline())):
-        s_time, e_time = tuple(map(int, sys.stdin.readline().split()))
+        conference.append(tuple(map(int, sys.stdin.readline().split())))    #s_time, e_time
+    conference.sort(key=lambda x:(x[1], x[0]))
+
+    cur_e_time = conference[0][1]
+    answer = 1
+
+    for s_time, e_time in conference[1:]:
+        if s_time >= cur_e_time and s_time <= e_time:
+            cur_e_time = e_time
+            answer += 1
+
+    print(answer)
+
+## 반례 ##
+'''
+3
+7 7
+8 8
+7 8
+
+answer : 3
+'''
