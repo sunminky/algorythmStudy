70185b499047ea1ed09beed226e54d51bd6b6104
sunminky <suns1502@gmail.com>
2021-02-07 18:13:08 +0900

    알쓰 11주차

--- 개인공부/새 스터디/11주차/Rank.py
+++ 개인공부/새 스터디/11주차/Rank.py
@@ -17,25 +17,18 @@ def solution(info, query):
         group[language[data[0]][0]][part[data[1]][0]][career[data[2]][0]][food[data[3]][0]].append(int(data[4]))
 
     #그룹별로 저장한 리스트 정렬
-    for l in range(len(language)-1):
-        for p in range(len(part)-1):
-            for c in range(len(career)-1):
-                for f in range(len(food)-1):
-                    group[l][p][c][f].sort()
+    for p in product(range(len(language)-1), range(len(part)-1), range(len(career)-1), range(len(food)-1)):
+        group[p[0]][p[1]][p[2]][p[3]].sort()
 
     #쿼리 전처리
     for q in query:
         cnt = 0
         raw_query = q.split()
         r_l, r_p, r_c, r_f = raw_query[::2]    #언어, 직군, 경력, 음식, 점수
-        r_l = language[r_l]
-        r_p = part[r_p]
-        r_c = career[r_c]
-        r_f = food[r_f]
         r_s = int(raw_query[-1])
 
         #카티션 프로덕트
-        for p in product(r_l, r_p, r_c, r_f):
+        for p in product(language[r_l], part[r_p], career[r_c], food[r_f]):
             # 그룹별 리스트의 길이 - 요구하는 점수보다 높은 점수가 제일 처음 나오는 위치 = 요구하는 점수보다 높은 지원자들의 개수
             cnt += len(group[p[0]][p[1]][p[2]][p[3]]) - bisect_left(group[p[0]][p[1]][p[2]][p[3]], r_s)
 
