2ad886490c629ecadd29eae3b86857b0cb1398c7
선민기 <suns1502@gmail.com>
2021-02-16 03:38:29 +0900

    알쓰 7주차

--- 알고리즘 스터디/3차/7주차/KevinBacon.py
+++ 알고리즘 스터디/3차/7주차/KevinBacon.py
@@ -3,8 +3,42 @@
 
 import sys
 
+def search(floyd):
+    global node
+
+    # x -> n -> y
+    for n in range(node):    #거치는 노드
+        for row in range(node): #모든 노드에 대해
+            for col in range(node): #모든 연결된 노드
+                if floyd[row][col] > floyd[row][n] + floyd[n][col]:
+                    floyd[row][col] = floyd[row][n] + floyd[n][col]
+
 if __name__ == '__main__':
+    global node
     node, edge = tuple(map(int, sys.stdin.readline().split()))
+    path = [[] for _ in range(node)]
+    floyd = [[100 for _ in range(node)] for _ in range(node)]   #다른 사용자까지의 거리를 저장
 
     for _ in range(edge):
-        tuple(map(int, sys.stdin.readline().split()))
+        src, dst = tuple(map(int, sys.stdin.readline().split()))
+        path[src-1].append(dst-1)
+
+    #인접 노드 채우기
+    for i in range(node):
+        floyd[i][i] = 0
+        for _path in path[i]:
+            floyd[i][_path] = floyd[_path][i] = 1
+
+    search(floyd)
+        
+    #값이 가장 작은 사용자 구하기
+    min_value = 100
+    winner = 1
+
+    for i in range(len(floyd)):
+        r_sum = sum(floyd[i])
+        if r_sum < min_value:
+            min_value = r_sum
+            winner = i + 1
+
+    print(winner)
