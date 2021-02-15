3c3d096cd5a7c2273319669b95e8fc7b10b3d41b
sunminky <suns1502@gmail.com>
2021-02-15 17:16:56 +0900

    알쓰 7주차

--- 알고리즘 스터디/3차/7주차/DownStair.py
+++ 알고리즘 스터디/3차/7주차/DownStair.py
@@ -1,34 +1,42 @@
 # https://www.acmicpc.net/problem/1520
 
 import sys
-from collections import deque
+sys.setrecursionlimit(500*500)
+
+movement = ((1, 0), (-1, 0), (0, 1), (0, -1))
+
+def search(my_map, visited, x, y) -> int:
+    answer = 0
+
+    #끝에 도달한 경우
+    if x == width-1 and y == height-1:
+        visited[y][x] = 1
+        return 1
+
+    #이미 방문한 곳인 경우
+    if visited[y][x] != -1:
+        return visited[y][x]
+
+    for _move in movement:
+        new_x = x + _move[0]
+        new_y = y + _move[1]
+
+        #바운더리 체크
+        if 0 <= new_x < width and 0 <= new_y < height:
+            #내리막길 체크
+            if my_map[y][x] > my_map[new_y][new_x]:
+                answer += search(my_map, visited, new_x, new_y)
+                visited_map[y][x] = answer  #도착지 까지 경로의 가지수 를 저장
+
+    return answer
 
 if __name__ == '__main__':
+    global height, width
     height, width = tuple(map(int, sys.stdin.readline().rstrip().split()))  # 지도 높이, 지도 너비
     my_map = []
-    count_map = [[0 for _ in range(width)] for _ in range(height)]
-    movement = ((1, 0), (-1, 0), (0, 1), (0, -1))
-    queue = deque()
+    visited_map = [[-1 for _ in range(width)] for _ in range(height)]    #방문 여부 저장
 
     for _ in range(height):
         my_map.append(list(map(int, sys.stdin.readline().rstrip().split())))
 
-    #큐에 출발지 넣음
-    queue.append((0, 0))
-    count_map[0][0] = 1
-
-    while queue:
-        cur_loc = queue.popleft()
-
-        for _move in movement:
-            x = cur_loc[0] + _move[0]
-            y = cur_loc[1] + _move[1]
-
-            #바운더리 체크
-            if 0 <= x < width and 0 <= y < height:
-                #내리막인지 체크
-                if my_map[y][x] < my_map[cur_loc[1]][cur_loc[0]]:
-                    count_map[y][x] += 1
-                    queue.append((x, y))
-
-    print(count_map[-1][-1])
+    print(search(my_map, visited_map, 0, 0))
