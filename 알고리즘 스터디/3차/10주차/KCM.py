fd9f1bda60c74120fb7f8b3ebfed60ee8a1021a2
sunminky <suns1502@gmail.com>
2021-02-22 18:29:40 +0900

    알쓰 10주차

--- 알고리즘 스터디/3차/10주차/KCM.py
+++ 알고리즘 스터디/3차/10주차/KCM.py
@@ -1,5 +1,5 @@
 #https://www.acmicpc.net/problem/10217
-#시간초과..
+#메모리초과..
 import sys
 import heapq
 
@@ -7,48 +7,30 @@ if __name__ == '__main__':
     n_testcase = int(sys.stdin.readline())
 
     for _ in range(n_testcase):
-        airport, support, ticket = tuple(map(int, sys.stdin.readline().split()))
-        path = [[] for _ in range(airport+1)] #간선 저장
-        fee = [[sys.maxsize for _ in range(airport+1)] for _ in range(support + 1)]   #요금 저장
-        visited = [False for _ in range(support + 1)]
-        dst_cost = []
-        queue = []
+        n_airport, support, ticket = tuple(map(int, sys.stdin.readline().split()))
+        path = [[] for _ in range(n_airport)]   #경로 저장
+        cost_table = [[support+1 for _ in range(support + 1)] for _ in range(n_airport)]    #각 노드별 소요시간 : 비용 저장
 
         for _ in range(ticket):
             src, dst, cost, time = tuple(map(int, sys.stdin.readline().split()))
-            path[src].append((dst, cost, time))
+            path[src-1].append((dst-1, cost, time))
 
-        visited[0] = True
-        fee[0][1] = 0
-        heapq.heappush(queue, 0)
-
+        queue = []
+        cost_table[0][0] = 0    #소요시간 0으로 1번 노드가는 비용 : 0
+        heapq.heappush(queue, (0, 0, 0))   #(시간, 비용, 노드)
+        #다익스트라
         while queue:
-            cur_cost = heapq.heappop(queue)
-            for ap in range(1, airport + 1):
-                if fee[cur_cost][ap] == sys.maxsize:
-                    continue
-                for neigh, cost, time in path[ap]:
-                    if cur_cost + cost > support:
-                        continue
-                    #값이 더 작으면 갱신
-                    if fee[cur_cost + cost][neigh] > fee[cur_cost][ap] + time:
-                        #사이클 감지
-                        cycle_flag = False
-                        for i in range(cur_cost):
-                            if fee[i][neigh] < fee[cur_cost][ap] + time:
-                                cycle_flag = True
-                                break
-                        if not cycle_flag:
-                            fee[cur_cost + cost][neigh] = fee[cur_cost][ap] + time
-                            if not visited[cur_cost + cost]:
-                                heapq.heappush(queue, cur_cost + cost)
-                                visited[cur_cost + cost] = True
-                            if neigh == airport:
-                                dst_cost.append(fee[cur_cost][ap] + time)
-        if not dst_cost:
-            print("Poor KCM")
+            c_time, c_cost, c_node = heapq.heappop(queue)
+
+            for n_dst, n_time, n_cost in path[c_node]:
+                if c_time + n_time <= support:
+                    if cost_table[n_dst][c_time + n_time] > c_cost + n_cost:
+                        cost_table[n_dst][c_time + n_time] = c_cost + n_cost
+                        heapq.heappush(queue, (c_time + n_time, c_cost + n_cost, n_dst))
+
+        answer = min(cost_table[-1])
+
+        if answer == support + 1:
+            print('Poor KCM')
         else:
-            print(min(dst_cost))
-
-        del fee, path, visited
-
+            print(answer)
