7225672e61ef2cd210e236a00d2c2660ec74d0fd
선민기 <suns1502@gmail.com>
2021-02-23 03:29:17 +0900

    알쓰 10주차

--- 알고리즘 스터디/3차/10주차/KCM.py
+++ 알고리즘 스터디/3차/10주차/KCM.py
@@ -1,36 +1,40 @@
-#https://www.acmicpc.net/problem/10217
-#메모리초과..
+# https://www.acmicpc.net/problem/10217
+# 메모리초과..
+
 import sys
-import heapq
+from queue import PriorityQueue
 
 if __name__ == '__main__':
     n_testcase = int(sys.stdin.readline())
 
     for _ in range(n_testcase):
         n_airport, support, ticket = tuple(map(int, sys.stdin.readline().split()))
-        path = [[] for _ in range(n_airport)]   #경로 저장
-        cost_table = [[support+1 for _ in range(support + 1)] for _ in range(n_airport)]    #각 노드별 소요시간 : 비용 저장
+        path = [[] for _ in range(n_airport)]  # 경로 저장
+        cost_table = [[10000001 for _ in range(support+1)] for _ in range(n_airport)] #cost_table[노드][비용] = 시간
 
         for _ in range(ticket):
             src, dst, cost, time = tuple(map(int, sys.stdin.readline().split()))
-            path[src-1].append((dst-1, cost, time))
+            path[src - 1].append((dst - 1, cost, time))
 
-        queue = []
-        cost_table[0][0] = 0    #소요시간 0으로 1번 노드가는 비용 : 0
-        heapq.heappush(queue, (0, 0, 0))   #(시간, 비용, 노드)
-        #다익스트라
-        while queue:
-            c_time, c_cost, c_node = heapq.heappop(queue)
+        queue = PriorityQueue()
+        cost_table[0][0] = 0
+        queue.put((0, 0, 0))    #시간, 비용, 노드
 
-            for n_dst, n_time, n_cost in path[c_node]:
-                if c_time + n_time <= support:
-                    if cost_table[n_dst][c_time + n_time] > c_cost + n_cost:
-                        cost_table[n_dst][c_time + n_time] = c_cost + n_cost
-                        heapq.heappush(queue, (c_time + n_time, c_cost + n_cost, n_dst))
+        while not queue.empty():
+            c_time, c_cost, c_node = queue.get()
+
+            if n_dst == n_airport - 1:
+                break
+
+            #이웃들 조사
+            for n_dst, n_cost, n_time in path[c_node]:
+                if c_cost + n_cost <= support:
+                    if cost_table[n_dst][c_cost+n_cost] > c_time+n_time:
+                        cost_table[n_dst][c_cost + n_cost] = c_time + n_time
+                        queue.put((c_time + n_time, c_cost + n_cost, n_dst))
 
         answer = min(cost_table[-1])
-
-        if answer == support + 1:
-            print('Poor KCM')
+        if answer != 10000001:
+            print(min(cost_table[-1]))
         else:
-            print(answer)
+            print("Poor KCM")
