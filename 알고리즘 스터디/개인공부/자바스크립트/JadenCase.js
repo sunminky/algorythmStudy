fd4e9277769e79c401fa89d77daa75475ef41284
선민기 <suns1502@gmail.com>
2021-02-25 00:15:41 +0900

    개인공부

--- 알고리즘 스터디/개인공부/자바스크립트/JadenCase.js
+++ 알고리즘 스터디/개인공부/자바스크립트/JadenCase.js
@@ -1,23 +1,21 @@
 /*https://programmers.co.kr/learn/courses/30/lessons/12951?language=python3*/
 
 function solution(s) {
-    var answer = Array.from(s.toLowerCase());
-    var pattern_eng = /[a-zA-Z]/;
-    var prev = answer[0]
+    var trans = s.toLowerCase().split(' ');
+    var answer = [];
+    var pattern_head = new RegExp("[a-zA-Z]+")
 
-    //첫 글자가 알파벳인지 체크
-    if(pattern_eng.test(answer[0]))
-        //알파벳인 경우 대문자로 바꿔줌
-        answer[0] = answer[0].toUpperCase()
+    for(var i=0;i<trans.length;i++){
+        var match = trans[i].match(pattern_head)
 
-    for(var i=1;i<answer.length;i++){
-        if(prev === ' ' && pattern_eng.test(answer[i]))
-            //이전 문자가 빈칸이고 알파벳인 경우 대문자로 바꿔줌
-            answer[i] = answer[i].toUpperCase()
-        prev = answer[i]
+        if (match === null)
+            answer.push(trans[i])
+        else {
+            answer.push(trans[i][0].toUpperCase() + trans[i].slice(1,))
+        }
     }
 
-    return answer.join('');
+    return answer.join(" ");
 }
 
 console.log(solution("3people unFollowed me"))
