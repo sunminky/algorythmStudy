5d386cc456053043a642040b9bb1a251ff1cda52
선민기 <suns1502@gmail.com>
2021-02-26 01:35:41 +0900

    개인공부

--- 알고리즘 스터디/개인공부/heap/Admission.py
+++ 알고리즘 스터디/개인공부/heap/Admission.py
@@ -1,21 +1,22 @@
 #https://programmers.co.kr/learn/courses/30/lessons/43238
 
-from queue import PriorityQueue
-
 def solution(n, times):
-    c_time = 0
-    cnt = n
-    queue = PriorityQueue()
+    max_time = 1000000000 * n + 1   #최악의 시간인 경우
+    min_time = 1    #최선의 시간인 경우
 
-    for _time in times:
-        queue.put((_time, _time))
+    while min_time < max_time:
+        middle_time = (max_time + min_time) // 2
 
-    while cnt != 0:
-        c_time, unit = queue.get()
-        cnt -= 1
-        queue.put((c_time+unit, unit))
+        #정해진 시간내 심사관들이 처리할 수 있는 사람들의 총 합 >= 처리해야 할 사람
+        if sum([middle_time // t for t in times]) >= n:
+            max_time = middle_time
+        else:
+            min_time = middle_time + 1
 
-    return c_time
+    return (min_time + max_time) // 2
+
 
 if __name__ == '__main__':
     solution(6, [7, 10])    #28
+    solution(10, [1, 5])    #9
+    solution(1, [1])  #1
