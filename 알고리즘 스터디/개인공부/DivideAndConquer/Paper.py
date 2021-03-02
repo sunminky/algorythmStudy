76800155564b773b3a7db8463496da1013a466f2
선민기 <suns1502@gmail.com>
2021-03-02 21:09:47 +0900

    개인공부

---
+++ 알고리즘 스터디/개인공부/DivideAndConquer/Paper.py
@@ -0,0 +1,34 @@
+#https://www.acmicpc.net/problem/2630
+
+import sys
+
+count = [0, 0]
+
+def divide(size:int, x:int, y:int):
+    global field
+
+    if size == 1:
+        count[field[y][x]] += 1
+
+    else:
+        sum_value = sum([sum(field[y+r][x:x+size]) for r in range(size)])
+
+        if sum_value == size ** 2 or sum_value == 0:
+            count[field[y][x]] += 1
+        else:
+            divide(size // 2, x, y)
+            divide(size // 2, x + size // 2, y)
+            divide(size // 2, x, y + size // 2)
+            divide(size // 2, x + size // 2, y + size // 2)
+
+if __name__ == '__main__':
+    global field
+    N = int(sys.stdin.readline())
+    field = [tuple(map(int, sys.stdin.readline().split())) for _ in range(N)]
+
+    divide(N // 2, 0, 0)
+    divide(N // 2, 0, N // 2)
+    divide(N // 2, N // 2, 0)
+    divide(N // 2, N // 2, N // 2)
+
+    print(count[0], count[1], sep="\n")
