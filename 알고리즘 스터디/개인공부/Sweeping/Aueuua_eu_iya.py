86690c812679e318b0de184e0eb5d31ecd171b6c
sunminky <suns1502@gmail.com>
2021-03-29 01:04:45 +0900

    개인공부

--- 알고리즘 스터디/개인공부/Sweeping/Aueuua_eu_iya.py
+++ 알고리즘 스터디/개인공부/Sweeping/Aueuua_eu_iya.py
@@ -3,4 +3,21 @@
 import sys
 
 if __name__ == '__main__':
-    sys.stdin.readline()
+    cur_x = cur_y = -1000000000
+    answer = 0
+
+    for _ in range(int(sys.stdin.readline())):
+        x, y = map(int, sys.stdin.readline().split())
+
+        # 이전 범위에서 벗어난 경우
+        if not cur_x <= x <= cur_y:
+            answer += cur_y - cur_x
+            cur_x = x
+            cur_y = y
+
+        else:
+            cur_y = max(cur_y, y)
+
+    # 마지막 구간 계산
+    answer += cur_y - cur_x
+    print(answer)
