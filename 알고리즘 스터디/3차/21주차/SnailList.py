7c28ad31239ce3edaf15d855c3bf8fba6e10094c
sunminky <suns1502@gmail.com>
2021-04-18 00:24:43 +0900

    알쓰21주차

--- 알고리즘 스터디/3차/21주차/SnailList.py
+++ 알고리즘 스터디/3차/21주차/SnailList.py
@@ -3,4 +3,15 @@
 import sys
 
 if __name__ == '__main__':
-    sys.stdin.readline()
+    n_node, n_query, final_dst = map(int, sys.stdin.readline().split())
+    nodes = [*map(int, sys.stdin.readline().split())]
+    tail = n_node - final_dst + 1
+    head = n_node - tail
+
+    for _ in range(n_query):
+        nth_node = int(sys.stdin.readline())
+
+        if nth_node < head:
+            print(nodes[nth_node])
+        else:
+            print(nodes[(nth_node - head) % tail + head])
