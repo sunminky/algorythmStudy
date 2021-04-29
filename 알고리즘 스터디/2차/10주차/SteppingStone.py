6d0a3f1518b8c9520dcca66ab2c1d98af05abbcc
sunminky <suns1502@gmail.com>
2021-04-29 17:08:11 +0900

    알쓰10주차

--- 알고리즘 스터디/2차/10주차/SteppingStone.py
+++ 알고리즘 스터디/2차/10주차/SteppingStone.py
@@ -1,42 +1,45 @@
+# https://programmers.co.kr/learn/courses/30/lessons/64062
+# 슬라이딩 윈도우, 최소값 큐
+
+
 from collections import deque
 
 
 def solution(stones, k):
-    kset = deque(stones[:k])
-    maxVal, count = maxCount(kset)
+    stepping_cnt = [0] * (len(stones) + 1)  # 모든 징검다리의 개수 + 도착 지점에 있는 땅
+    sliding_window = deque()  # 슬라이딩 윈도우
+    max_queue = deque()  # 최대값 저장 큐
 
-    for step in range(k, len(stones)):
+    sliding_window.append(200000000)  # 징검다리를 밟기전 땅의 숫자(무한번 밟기 가능)
+    max_queue.append(200000000)
+    stones.append(200000000)    # 징검다리를 건넌 후 땅의 숫자(무한번 밟기 가능)
 
-        if stones[step] <= maxVal:
-            kset.append(stones[step])
-        else:
-            kset.append(maxVal)
-            count = count + 1
+    for seq, stone in enumerate(stones):
+        # 슬라이딩 윈도우에 원소 추가
+        if len(sliding_window) > k:
+            del_stone = sliding_window.popleft()
 
-        if kset[0] == maxVal:
-            count = count - 1
-            kset.popleft()
-            if count == 0:
-                maxVal, count = maxCount(kset)
-        else:
-            kset.popleft()
+            if del_stone == max_queue[0]:
+                max_queue.popleft()
 
-    return maxVal
+        stepping_cnt[seq] = min(max_queue[0], stones[seq])
 
-def maxCount(kset):
-    maxVal = 0
-    count = 0
-    for i in kset:
-        if i > maxVal:
-            maxVal = i
-            count = 0
-        if i == maxVal:
-            count = count + 1
+        # 최대큐에서 자기보다 작은 값은 제거
+        while max_queue:
+            if max_queue[-1] < stepping_cnt[seq]:
+                max_queue.pop()
+            else:
+                break
 
-    return maxVal, count
+        # 현재 돌 추가
+        max_queue.append(stepping_cnt[seq])
+        sliding_window.append(stepping_cnt[seq])
+
+    return stepping_cnt[-1]     # 도착지 땅에 발을 디딜수 있는 횟수 반환
+
 
 if __name__ == '__main__':
     result = solution([2, 4, 5, 3, 2, 1, 4, 2, 5, 1], 3)
     print(result)
-    result = solution([i for i in range(10**12, 0, -1)], 10**6)
+    result = solution([i for i in range(200000, 0, -1)], 200000)
     print(result)
