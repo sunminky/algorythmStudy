57976d920b7720fc31a004764bf758656a81a859
sunminky <suns1502@gmail.com>
2021-05-01 22:18:49 +0900

    알쓰22주차

--- 알고리즘 스터디/3차/22주차/Gear2.py
+++ 알고리즘 스터디/3차/22주차/Gear2.py
@@ -3,38 +3,40 @@
 import sys
 
 
+gear = [list(sys.stdin.readline().rstrip()) for _ in range(int(sys.stdin.readline()))]
+
+
 def rotate(gear, direction) -> list:
     if direction == 1:
         return [gear[-1]] + gear[:-1]
     return gear[1:] + [gear[0]]
 
 
-if __name__ == '__main__':
-    gear = [list(sys.stdin.readline().rstrip()) for _ in range(int(sys.stdin.readline()))]
+def compare(flag, nth_gear, direction):
+    cmp_range = ((nth_gear - 2, -1, -1), (nth_gear, len(gear), 1))
+    cmp_gear = ((6, 2), (2, 6))
+    cmp_idx = (1, -1)
+    gear_copy = gear.copy()
 
+    cur_direction = 0 if direction == 1 else 1
+    for seq in range(*cmp_range[flag]):
+        # seq + 1의 7번째 톱니와 seq의 3번째 톱니 비교
+        if gear_copy[seq + cmp_idx[flag]][cmp_gear[flag][0]] == gear_copy[seq][cmp_gear[flag][1]]:
+            break
+
+        gear[seq] = rotate(gear[seq], cur_direction)
+        cur_direction = 0 if cur_direction == 1 else 1
+
+
+if __name__ == '__main__':
     for _ in range(int(sys.stdin.readline())):
         nth_gear, direction = map(int, sys.stdin.readline().split())
-        gear_copy = gear.copy()
         
         # nth 기어보다 앞에 있는 바퀴
-        cur_direction = 0 if direction == 1 else 1
-        for seq in range(nth_gear - 2, -1, -1):
-            # seq + 1의 7번째 톱니와 seq의 3번째 톱니 비교
-            if gear_copy[seq + 1][6] == gear_copy[seq][2]:
-                break
-
-            gear[seq] = rotate(gear[seq], cur_direction)
-            cur_direction = 0 if cur_direction == 1 else 1
+        compare(0, nth_gear, direction)
 
         # nth 기어보다 뒤에 있는 바퀴
-        cur_direction = 0 if direction == 1 else 1
-        for seq in range(nth_gear, len(gear)):
-            # seq - 1의 3번째 톱니와 seq의 7번째 톱니 비교
-            if gear_copy[seq - 1][2] == gear_copy[seq][6]:
-                break
-
-            gear[seq] = rotate(gear[seq], cur_direction)
-            cur_direction = 0 if cur_direction == 1 else 1
+        compare(1, nth_gear, direction)
             
         # nth 기어 돌리기
         gear[nth_gear-1] = rotate(gear[nth_gear-1], direction)
