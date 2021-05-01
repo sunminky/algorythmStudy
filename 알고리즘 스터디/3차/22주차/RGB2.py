d80b69f931bea21234afe70d4f051b132c5d8458
sunminky <suns1502@gmail.com>
2021-05-01 22:25:01 +0900

    알쓰22주차

--- 알고리즘 스터디/3차/22주차/RGB2.py
+++ 알고리즘 스터디/3차/22주차/RGB2.py
@@ -7,19 +7,17 @@ if __name__ == '__main__':
     answer = 10000000
 
     for i in range(3):
-        estimate = [[0] * 3 for _ in range(len(cost))]
-
-        for j in range(3):
-            estimate[1][j] = cost[0][i] + cost[1][j]
-
-        estimate[1][i] = 2001
+        estimate = [cost[0][i] + cost[1][j] for j in range(3)]
+        estimate[i] = 2001
 
         for j in range(2, len(cost)):
-            estimate[j][0] = cost[j][0] + min(estimate[j-1][1], estimate[j-1][2])
-            estimate[j][1] = cost[j][1] + min(estimate[j - 1][0], estimate[j - 1][2])
-            estimate[j][2] = cost[j][2] + min(estimate[j - 1][0], estimate[j - 1][1])
+            estimate = [
+                cost[j][0] + min(estimate[1], estimate[2]),
+                cost[j][1] + min(estimate[0], estimate[2]),
+                cost[j][2] + min(estimate[0], estimate[1])
+            ]
 
-        estimate[-1][i] = 10000000
-        answer = min(answer, min(estimate[-1]))
+        estimate[i] = 10000000
+        answer = min(answer, min(estimate))
 
     print(answer)
