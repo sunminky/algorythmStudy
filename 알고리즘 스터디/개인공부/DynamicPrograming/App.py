d9c560ff7602ed5aa7ce1f62636a5053f56e8157
sunminky <suns1502@gmail.com>
2021-05-15 14:05:03 +0900

    개인공부

--- 알고리즘 스터디/개인공부/DynamicPrograming/App.py
+++ 알고리즘 스터디/개인공부/DynamicPrograming/App.py
@@ -1,27 +1,21 @@
-#https://www.acmicpc.net/problem/7579
-
+# https://www.acmicpc.net/problem/7579
 import sys
 
 if __name__ == '__main__':
     N, mem_limit = tuple(map(int, sys.stdin.readline().split()))
     mem = tuple(map(int, sys.stdin.readline().split()))
     cost = tuple(map(int, sys.stdin.readline().split()))
-    sum_cost = sum(cost)
-    knapsack = [-1 for _ in range(sum_cost+1)]
-    answer = sum_cost
-
-    knapsack[0] = 0  #아무것도 안챙김
+    knapsack = {0: 0}   # 아무것도 안챙긴 경우가 기본값
+    answer = sum_cost = sum(cost)
 
     for i in range(N):
         prev_knapsack = knapsack.copy()
 
-        for j in range(sum_cost+1):
-            if prev_knapsack[j] != -1:
-                knapsack[j + cost[i]] = max(knapsack[j + cost[i]], prev_knapsack[j] + mem[i])
+        for key in prev_knapsack:
+            knapsack[key + cost[i]] = max(knapsack.get(key + cost[i], 0), prev_knapsack[key] + mem[i])
 
-    for i in range(sum_cost + 1):
-        if knapsack[i] != -1 and knapsack[i] >= mem_limit:
-            answer = i
-            break
+    for key in knapsack:
+        if knapsack[key] >= mem_limit:
+            answer = min(answer, key)
 
     print(answer)
