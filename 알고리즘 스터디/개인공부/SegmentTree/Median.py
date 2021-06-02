cf1fc3546b35a8f52a03aaf4806870575ee724d4
sunminky <suns1502@gmail.com>
2021-06-02 14:02:46 +0900

    개인공부

--- 알고리즘 스터디/개인공부/SegmentTree/Median.py
+++ 알고리즘 스터디/개인공부/SegmentTree/Median.py
@@ -3,49 +3,54 @@
 import sys
 
 
-class PenwikTree:
-    def __init__(self, size):
-        self.tree = [0] * (size + 1)
+def inflate_tree():
+    # height = 17     # ceil(log2(65536 + 1))
+    tree = [0] * 262144     # (2 << height)
 
-    def update(self, nth_node, value):
-        while nth_node < len(self.tree):
-            self.tree[nth_node] += value
-            nth_node += nth_node & -nth_node
+    return tree
 
-    def search(self, nth_node):
-        result = 0
 
-        while nth_node > 0:
-            result += self.tree[nth_node]
-            nth_node -= nth_node & -nth_node
+def update(tree, nth_node, value):
+    nth_node += 131072    # 1 << height
+    # Bottom-Up
+    while nth_node:
+        tree[nth_node] += value
+        nth_node >>= 1
 
-        return result
+
+def search(tree, limit):
+    query_start = 0
+    query_end = 131071  # (1 << height) - 1
+    nth_node = 1
+    remain = limit
+
+    # Top-Down
+    while query_start < query_end:
+        middle = (query_start + query_end) >> 1
+
+        if tree[nth_node * 2] < remain:
+            remain -= tree[nth_node * 2]
+            query_start = middle + 1
+            nth_node = nth_node * 2 + 1
+        else:
+            query_end = middle
+            nth_node <<= 1
+
+    return query_start
 
 
 if __name__ == '__main__':
     n_number, width = map(int, sys.stdin.readline().split())
     numbers = tuple(int(sys.stdin.readline()) for _ in range(n_number))
-    tree = PenwikTree(65536)
+    tree = inflate_tree()
     answer = 0
 
     for i in range(width - 1):
-        tree.update(numbers[i], 1)
+        update(tree, numbers[i], 1)
 
     for i in range(n_number - width + 1):
-        tree.update(numbers[i + width - 1], 1)
-        # 이분 탐색 #
-        start = 1
-        end = 131072    # 65536 * 2
-        while start < end:
-            middle = (start + end) // 2
-
-            if tree.search(middle) < (width + 1) // 2:
-                start = middle + 1
-            else:
-                end = middle
-
-        answer += end
-
-        tree.update(numbers[i], -1)
+        update(tree, numbers[i + width - 1], 1)
+        answer += search(tree, (width + 1) >> 1)
+        update(tree, numbers[i], -1)
 
     print(answer)
