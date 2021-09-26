bfd9a7f780c006a968503d7843981d9c9b73b9f1
sunminky <suns1502@gmail.com>
2021-09-26 22:16:28 +0900

    개인공부

--- 알고리즘 스터디/개인공부/Sorting/LargestNumber.py
+++ 알고리즘 스터디/개인공부/Sorting/LargestNumber.py
@@ -5,24 +5,24 @@ import sys
 
 def matching(values: list) -> list:
     matched = []
-    max_digit = len(max(values, key=len)) + 1   # 가장 긴 숫자의 길이
+    max_digit = len(max(values, key=len)) + 1  # 가장 긴 숫자의 길이
 
     for value in values:
-        calc = [value[0]] * max_digit   # 가장 맨 앞자리 값 * 가장 긴 숫자의 길이
+        calc = [value[0]] * max_digit  # 가장 맨 앞자리 값 * 가장 긴 숫자의 길이
 
         # 앞에서 부터 차례대로 넣어줌
         for i in range(len(value)):
             calc[i] = value[i]
 
-        matched.append([int("".join(calc)), value])     # [변환 값, 입력받은 값]
+        matched.append([int("".join(calc)), value])  # [변환 값, 입력받은 값]
 
     return matched
 
 
 if __name__ == '__main__':
-    sys.stdin.readline()    # 필요없음
+    sys.stdin.readline()  # 필요없음
     answer = [e[1] for e in sorted(matching(sys.stdin.readline().split()),
-                                   key=lambda x: (x[0], x[1][-1]),  # 변환 값이 같을 경우 마지막 문자를 따라 정렬
+                                   key=lambda x: (x[0], x[1][-1], -len(x[1])),  # 변환 값이 같을 경우 마지막 문자를 따라 정렬
                                    reverse=True)]
     print(int("".join(answer)))
 
