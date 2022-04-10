b3f3310fc90ffb2267e6d37eac865e28becf0f0d
sunminky <suns1502@gmail.com>
2022-04-10 14:03:46 +0900

    개인공부

--- 알고리즘 스터디/개인공부/SegmentTree/NewStaff.py
+++ 알고리즘 스터디/개인공부/SegmentTree/NewStaff.py
@@ -3,24 +3,19 @@ import sys
 
 if __name__ == '__main__':
     for _ in range(int(sys.stdin.readline())):
-        n_candidate = int(sys.stdin.readline())
-        rank = [list() for _ in range(2)]
+        cur_idx = n_candidate = int(sys.stdin.readline())
+        rank = [[0] * n_candidate for _ in range(2)]
         answer = 0
-        cur_idx = n_candidate
 
         for seq in range(n_candidate):
             _rank1, _rank2 = map(int, sys.stdin.readline().split())
-            rank[0].append((seq + 1, _rank1))
-            rank[1].append((seq + 1, _rank2))
+            rank[0][_rank1 - 1] = rank[1][_rank2 - 1] = seq
 
-        rank[0].sort(key=lambda x: x[1])
-        rank[1].sort(key=lambda x: x[1])
+        rank_dict = dict(zip(rank[1], range(n_candidate)))
 
-        rank_dict = dict(zip((e[0] for e in rank[1]), range(n_candidate)))
-
-        for seq in range(n_candidate):
-            if rank_dict[rank[0][seq][0]] < cur_idx:
-                cur_idx = rank_dict[rank[0][seq][0]]
+        for e in rank[0]:
+            if rank_dict[e] < cur_idx:
+                cur_idx = rank_dict[e]
                 answer += 1
 
         print(answer)
