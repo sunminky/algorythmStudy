a0ae9fc47faef22ef639b78bc986e537a78a7c41
sunminky <suns1502@gmail.com>
2022-05-23 17:03:12 +0900

    개인공부

--- 알고리즘 스터디/개인공부/BackTracking/NQueen.py
+++ 알고리즘 스터디/개인공부/BackTracking/NQueen.py
@@ -2,47 +2,32 @@
 import sys
 
 width = int(sys.stdin.readline())
-field = [[True] * width for _ in range(width)]
-visited = [[False] * width for _ in range(width)]
-x_visited = [False] * width
-y_visited = [False] * width
+y_pos = [False] * width
+visited = []
 
 
-def deploy(remain):
+def deploy(remain) -> int:
     result = 0
 
-    # 모든 queen을 다 놓음
+    # 퀸을 다 놓음
     if remain == 0:
         return 1
 
-    # n 번째 퀸, n 번째 칸
     for row in range(width):
-        is_fail = False
-        x_plus_y = row + remain - 1
-        x_minus_y = remain - 1 - row
-
-        # 가로 체크
-        if y_visited[row]:
+        # 세로 체크
+        if y_pos[row]:
             continue
         # 대각선 체크
-        for row2 in range(max(0, x_plus_y - width + 1), min(x_plus_y + 1, width)):
-            if visited[row2][x_plus_y - row2]:
-                is_fail = True
+        for e in visited:
+            if (e[0] - width + remain) ** 2 == (e[1] - row) ** 2:
                 break
         else:
-            # 대각선 체크
-            for row2 in range(row - min(row, remain - 1), min(width - x_minus_y, width)):
-                if visited[row2][row2 + x_minus_y]:
-                    is_fail = True
-                    break
-
-        if not is_fail:
             # 놓기
-            x_visited[remain - 1] = y_visited[row] = visited[row][remain - 1] = True
-            # 다음 퀸 배치
+            visited.append((width - remain, row))
+            y_pos[row] = True
             result += deploy(remain - 1)
-            # 복구하기
-            x_visited[remain - 1] = y_visited[row] = visited[row][remain - 1] = False
+            del visited[-1]
+            y_pos[row] = False
 
     return result
 
