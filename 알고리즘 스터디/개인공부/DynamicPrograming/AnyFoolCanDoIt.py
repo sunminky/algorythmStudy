c2599e12e26f46cc5f49822b4690d312464f5538
sunminky <suns1502@gmail.com>
2022-05-24 18:42:31 +0900

    개인공부

--- 알고리즘 스터디/개인공부/DynamicPrograming/AnyFoolCanDoIt.py
+++ 알고리즘 스터디/개인공부/DynamicPrograming/AnyFoolCanDoIt.py
@@ -5,7 +5,7 @@ import sys
 def find_comma(text) -> list:
     result = []
 
-    for i in range(len(text)):
+    for i in range(1, len(text)):
         if text[i] == ",":
             result.append(i)
 
@@ -17,47 +17,37 @@ def is_set(set_dp, text, comma, start, end) -> bool:
         return set_dp[start][end]
 
     if text[start] == "{" and text[end] == "}":
+        # 둘이 붙어 있음
         if start + 1 == end:
             set_dp[start][end] = True
             return True
 
-        if start + 2 == end:
-            set_dp[start][end] = True
-            return True
-
+        # 내부가 Element 인지 확인
         if is_set(set_dp, text, comma, start + 1, end - 1):
             set_dp[start][end] = True
             return True
 
-        set_dp[start][end] = False
-
         # 콤마단위로 구분
         for e in comma:
             if start + 1 <= e <= end - 1:
-                if is_element(set_dp, text, comma, start + 1, e - 1) and is_element(set_dp, text, comma,
-                                                                                    e + 1, end - 1):
+                if is_element(set_dp, text, comma, start + 1, e - 1) \
+                        and is_element(set_dp, text, comma, e + 1, end - 1):
                     set_dp[start][end] = True
-                    break
+                    return True
 
-    return set_dp[start][end]
+    set_dp[start][end] = False
+    return False
 
 
 def is_element(set_dp, text, comma, start, end) -> bool:
+    if start > end:
+        return False
+
     if set_dp[start][end] is not None:
         return set_dp[start][end]
 
-    if start > end:
-        set_dp[start][end] = False
-        return False
-
-    if start == end:
-        set_dp[start][end] = True
-        return True
-
     if is_set(set_dp, text, comma, start, end):
         return True
-
-    set_dp[start][end] = False
 
     # 콤마단위로 구분
     for e in comma:
@@ -65,9 +55,10 @@ def is_element(set_dp, text, comma, start, end) -> bool:
             if is_element(set_dp, text, comma, start, e - 1) and is_element(set_dp, text, comma,
                                                                             e + 1, end):
                 set_dp[start][end] = True
-                break
+                return True
 
-    return set_dp[start][end]
+    set_dp[start][end] = False
+    return False
 
 
 if __name__ == '__main__':
@@ -76,7 +67,10 @@ if __name__ == '__main__':
         comma = find_comma(text)
         set_dp = [[None for _ in range(len(text))] for _ in range(len(text))]
 
-        if is_set(set_dp, text, comma, 0, len(text) - 1):
+        for row in range(len(text)):
+            set_dp[row][row] = True
+
+        if len(text) != 1 and is_set(set_dp, text, comma, 0, len(text) - 1):
             print(f"Word #{seq + 1}: Set")
         else:
             print(f"Word #{seq + 1}: No Set")
