3d956101b190663d7ffa522a49cf59db16c78a11
sunminky <suns1502@gmail.com>
2022-05-31 11:44:44 +0900

    개인공부

--- 알고리즘 스터디/개인공부/Greedy/CharacterMath.py
+++ 알고리즘 스터디/개인공부/Greedy/CharacterMath.py
@@ -1,27 +1,27 @@
 # https://www.acmicpc.net/problem/1339
-import operator
 import sys
 
 if __name__ == '__main__':
-    digit_cnt = [dict() for _ in range(8)]  # 자리수별 알파벳 등장횟수 저장
-    alphabet_dict = dict()  # 알파벳별 숫자값 저장
-    num = [sys.stdin.readline().rstrip() for _ in range(int(sys.stdin.readline().rstrip()))]
+    words = [sys.stdin.readline().rstrip() for _ in range(int(sys.stdin.readline()))]
+    count_dict = [dict() for _ in range(9)]
+    alpha_dict = dict()
+    answer = 0
 
-    # 자리수 별 알파벳 등장횟수 저장
-    for _num in num:
-        for i in range(len(_num)):
-            digit_cnt[-i - 1][_num[-i - 1]] = digit_cnt[-i - 1].get(_num[-i - 1], 0) + 1
+    for e in words:
+        for seq, ch in enumerate(e):
+            count_dict[len(count_dict) - len(e) + seq][ch] = count_dict[len(count_dict) - len(e) + seq].get(ch, 0) + 1
 
-    # 낮은 자리수에서 등장한 알파벳의 가중치를 상위 자리수에 더해줌
-    for i in range(len(digit_cnt) - 1):
-        if digit_cnt[-i - 2]:
-            for e in digit_cnt[-i - 1]:
-                digit_cnt[-i - 2][e] = digit_cnt[-i - 2].get(e, 0) + digit_cnt[-i - 1][e] / 10
+    for i in range(len(count_dict) - 1, 0, -1):
+        for key in count_dict[i]:
+            count_dict[i - 1][key] = count_dict[i - 1].get(key, 0) + count_dict[i][key] / 10
+            count_dict[i][key] %= 10
 
-    # 높은 자리부터 등장 빈도가 높은 알파벳을 큰 숫자로 바꿔줌
-    for e in digit_cnt:
-        for key, _ in sorted(e.items(), key=operator.itemgetter(1), reverse=True):   # 딕셔너리 value로 정렬
-            if alphabet_dict.get(key, False) is False:
-                alphabet_dict[key] = 9 - len(alphabet_dict)
+    table = sorted(count_dict[0].items(), key=lambda x: x[1], reverse=True)
 
-    print(sum([int("".join(map(lambda x: str(alphabet_dict[x]), e))) for e in num]))
+    for i in range(min(10, len(table))):
+        alpha_dict[table[i][0]] = str(9 - i)
+
+    for word in words:
+        answer += int("".join((map(lambda x: alpha_dict[x], word))))
+
+    print(answer)
