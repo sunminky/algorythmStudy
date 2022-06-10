4a7b2df67d196910cb0ccd3b02922aaa2c9239f4
sunminky <suns1502@gmail.com>
2022-06-10 17:12:00 +0900

    개인공부

--- 알고리즘 스터디/개인공부/Bitmask/RobotCleaner.py
+++ 알고리즘 스터디/개인공부/Bitmask/RobotCleaner.py
@@ -4,97 +4,100 @@ import sys
 from collections import deque
 
 
-def search(distance, visited, cost, cur_node):
-    # 이미 방문한 노드인 경우
-    if cost[cur_node][visited] != -1:
-        return cost[cur_node][visited]
-    else:
-        cost[cur_node][visited] = 40001
-        for i in range(len(distance)):
-            # 이미 방문한 노드인 경우
-            if visited & (1 << i):
-                continue
-            cost[cur_node][visited] = \
-                min(cost[cur_node][visited], search(distance, visited | (1 << i), cost, i) + distance[cur_node][i])
+# 먼지 위치 구하기
+def get_stain(width, height, field):
+    stain_dict = dict()
+    start = None
 
-        return cost[cur_node][visited]
+    for row in range(height):
+        for col in range(width):
+            if field[row][col] == "*":
+                stain_dict[(col, row)] = len(stain_dict)
+            elif field[row][col] == "o":
+                start = (col, row)
+
+    stain_dict[start] = len(stain_dict)
+
+    return stain_dict
 
 
-# 외판원 순회
-def tsp(distance, stain, start):
-    cost = [[-1 for _ in range(1 << len(stain))] for _ in range(len(stain))]
-    starting = 1 << stain[start]  # 청소기 위치 방문
-    all_visited = (1 << len(stain)) - 1
+# 먼지간 거리 구하기
+def get_distance(width, height, field, stain):
+    distance = [dict() for _ in range(len(stain))]
+    movement = ((1, 0), (-1, 0), (0, 1), (0, -1))
 
-    # 모든 노드를 방문한 경우에는 0으로 세팅(다시 출발지로 돌아갈 필요가 없기때문에)
-    for i in range(len(cost)):
-        cost[i][all_visited] = 0
+    for seq, node in enumerate(stain.keys()):
+        queue = deque([(*node, 0)])
+        visited = [[False] * width for _ in range(height)]
 
-    return search(distance, starting, cost, stain[start])
+        visited[node[1]][node[0]] = True
+
+        while queue:
+            cur_x, cur_y, cur_dist = queue.popleft()
+
+            for _move in movement:
+                new_x = cur_x + _move[0]
+                new_y = cur_y + _move[1]
+
+                # 바운더리
+                if not 0 <= new_x < width:
+                    continue
+                if not 0 <= new_y < height:
+                    continue
+                if visited[new_y][new_x]:
+                    continue
+                if field[new_y][new_x] == "x":
+                    continue
+
+                queue.append((new_x, new_y, cur_dist + 1))
+                visited[new_y][new_x] = True
+
+                if field[new_y][new_x] == "*":
+                    distance[seq][stain[(new_x, new_y)]] = cur_dist + 1
+
+        # 먼지에서 시작지점으로 가는 비용은 0
+        distance[seq][len(stain) - 1] = 0
+
+    return distance
+
+
+def tsp(cur_visited, cur_node, dp, distance):
+    # 이미 계산한 적이 있다면 계산한 값 리턴
+    if dp[cur_node][cur_visited] is not None:
+        return dp[cur_node][cur_visited]
+
+    # 방문 안한 지점 탐색
+    dp[cur_node][cur_visited] = 401
+    for i in range(len(distance) - 1):
+        if cur_visited & (1 << i):
+            continue
+        else:
+            dp[cur_node][cur_visited] = min(dp[cur_node][cur_visited],
+                                            tsp(cur_visited | (1 << i), i, dp, distance) + distance[cur_node][i])
+
+    return dp[cur_node][cur_visited]
 
 
 if __name__ == '__main__':
     while True:
         width, height = map(int, sys.stdin.readline().split())
-        field = [sys.stdin.readline().rstrip() for _ in range(height)]
-        stain = dict()  # 청소기, 쓰레기 좌표 저장
-        start = None    # 청소기 위치 저장
-        answer = -1
 
-        if not (width and height):  # 둘 다 0 이면 종료
+        if width == 0 and height == 0:
             break
 
-        # 청소기, 쓰레기 좌표 탐색
-        for row in range(height):
-            for col in range(width):
-                if field[row][col] == '*' or field[row][col] == 'o':
-                    stain[(col, row)] = len(stain)
-                if field[row][col] == 'o':
-                    start = (col, row)
+        field = [sys.stdin.readline().rstrip() for _ in range(height)]
+        stain = get_stain(width, height, field) # 먼지 위치
+        distance = get_distance(width, height, field, stain)    # 먼지간 거리
+        all_visited = (1 << (len(stain) - 1)) - 1   # 모든 먼지를 방문했을 때 비트마스킹
+        dp = [[None] * (1 << (len(stain) - 1)) for _ in range(len(stain))]
 
-        distance = [[0 for _ in range(len(stain))] for _ in range(len(stain))]  # 쓰레기들의 거리 저장
+        for row in range(len(stain)):
+            dp[row][all_visited] = 0
 
-        # 0(청소기) ~ 마지막 먼지 까지 거리 구하기
-        for col, row in stain:
-            visited = [[False for _ in range(width)] for _ in range(height)]
-            queue = deque()
-            queue.append((col, row, 0))
-            visited[row][col] = True
-            stain_cnt = len(stain)  # 도달한 먼지의 개수 카운트
-
-            # 먼지 간 거리 구하기
-            while queue:
-                c_col, c_row, c_dis = queue.popleft()
-
-                if field[c_row][c_col] == '*' or field[c_row][c_col] == 'o':
-                    distance[stain[(col, row)]][stain[(c_col, c_row)]] = \
-                        distance[stain[(c_col, c_row)]][stain[(col, row)]] = c_dis
-                    stain_cnt -= 1
-
-                if c_col - 1 >= 0 and field[c_row][c_col - 1] != 'x':
-                    if visited[c_row][c_col - 1] is False:
-                        visited[c_row][c_col - 1] = True
-                        queue.append((c_col - 1, c_row, c_dis + 1))
-                if c_col + 1 < width and field[c_row][c_col + 1] != 'x':
-                    if visited[c_row][c_col + 1] is False:
-                        visited[c_row][c_col + 1] = True
-                        queue.append((c_col + 1, c_row, c_dis + 1))
-                if c_row - 1 >= 0 and field[c_row - 1][c_col] != 'x':
-                    if visited[c_row - 1][c_col] is False:
-                        visited[c_row - 1][c_col] = True
-                        queue.append((c_col, c_row - 1, c_dis + 1))
-                if c_row + 1 < height and field[c_row + 1][c_col] != 'x':
-                    if visited[c_row + 1][c_col] is False:
-                        visited[c_row + 1][c_col] = True
-                        queue.append((c_col, c_row + 1, c_dis + 1))
-
-            # 도달할 수 없는 먼지가 있는 경우
-            if stain_cnt != 0:
-                break
-
-        # 닿을 수 없는 먼지가 존재하지 않음
+        # 시작지점에서 도달할 수 없는 먼지가 존재하는 경우
+        if len(distance[-1]) != len(stain):
+            answer = -1
         else:
-            # 외판원 순회 실행
-            answer = tsp(distance, stain, start)
+            answer = tsp(0, len(stain) - 1, dp, distance)
 
         print(answer)
