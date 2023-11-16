0c66f70a0fc33cfa2435da1d58ea511aad1e7be8
sunminky <suns1502@gmail.com>
2023-11-16 19:43:37 +0900

    개인공부

--- 알고리즘 스터디/개인공부/BFS&DFS/WeightLimit.py
+++ 알고리즘 스터디/개인공부/BFS&DFS/WeightLimit.py
@@ -4,29 +4,27 @@ import heapq
 
 if __name__ == '__main__':
     n_island, n_bridge = map(int, sys.stdin.readline().split())
-    island_dict = [dict() for _ in range(n_island)]
-    costs = [0] * n_island
+    path = [dict() for _ in range(n_island)]
+    limit = [0] * n_island
     queue = []
 
     for _ in range(n_bridge):
-        src, dst, limit = map(int, sys.stdin.readline().split())
+        _island1, _island2, _limit = map(int, sys.stdin.readline().split())
+        path[_island1 - 1][_island2 - 1] = max(path[_island1 - 1].get(_island2 - 1, 0), _limit)
+        path[_island2 - 1][_island1 - 1] = max(path[_island2 - 1].get(_island1 - 1, 0), _limit)
 
-        island_dict[src - 1][dst - 1] = max(island_dict[src - 1].get(dst - 1, 0), limit)
-        island_dict[dst - 1][src - 1] = max(island_dict[dst - 1].get(src - 1, 0), limit)
+    src, dst = map(int, sys.stdin.readline().split())
 
-    start, end = map(int, sys.stdin.readline().split())
-    heapq.heappush(queue, (-1000000000, start - 1))  # (노드, 비용)
-    costs[start - 1] = 1000000000
+    heapq.heappush(queue, (-1000000001, src - 1))
 
     while queue:
-        cur_cost, cur_node = heapq.heappop(queue)
+        cur_cost, cur_island = heapq.heappop(queue)
 
-        if cur_node == end - 1:
-            break
+        for _neigh in path[cur_island]:
+            _limit = min(-cur_cost, path[cur_island][_neigh])
 
-        for _neigh in island_dict[cur_node].keys():
-            if costs[_neigh] < min(island_dict[cur_node][_neigh], -cur_cost):
-                costs[_neigh] = min(island_dict[cur_node][_neigh], -cur_cost)
-                heapq.heappush(queue, (-costs[_neigh], _neigh))
+            if _limit > limit[_neigh]:
+                limit[_neigh] = _limit
+                heapq.heappush(queue, (-_limit, _neigh))
 
-    print(costs[end - 1])
+    print(limit[dst - 1])
